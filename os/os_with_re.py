56633d84c514f87e64593a76e6dcf64fa7be2e47
KaziFarhan <kazifarhan199@gmail.com>
2017-05-29 17:56:16 +0530

    Create os_with_re.py

---
+++ os/os_with_re.py
@@ -0,0 +1,21 @@
+import os 
+import time
+import re
+
+a=os.getcwd()			#get current working directory
+print(a)
+b=input('Where to go next? ')
+
+c=re.sub(r'\\',r'\\\\',b)
+os.chdir(c)				#change working directery
+
+print(os.getcwd())
+
+os.mkdir('The name of new directory')			#make directory
+time.sleep(4)
+os.rename('The name of new directory','new name')	#As rename has path and name to move 
+time.sleep(4)									#it to other directory just change the path
+												#eg:"os.rename(old//path//a,new//path//a)"
+
+os.rmdir('new name')								#Remove directory
+												
