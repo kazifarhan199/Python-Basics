fbdfd5f52c1ab3e1baebb08120639504c27e1fb9
kazifarhan <kazifarhan199@gmmail.com>
2017-08-15 14:47:51 +0530

    updated

---
+++ Kivy_basics/basic_kivy.py
@@ -0,0 +1,29 @@
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+#kivy.require("1.10.0")
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+
+class login_screen(GridLayout):
+	def __init__(self,**kwargs):
+		super(login_screen,self).__init__(**kwargs)
+		self.cols=2
+		
+		self.add_widget(Label(text="Name: "))
+		self.username=TextInput(multiline=False)
+		self.add_widget(self.username)
+
+		self.add_widget(Label(text="Passward: "))
+		self.password=TextInput(multiline=False,password=True)
+		self.add_widget(self.password)
+		
+		
+class kivy_app(App):
+	def build(self):
+		return login_screen()
+
+
+
+if __name__=="__main__":
+	kivy_app().run()
