773508725b714c34aeb34e1fe7abe3c9cff6b713
KaziFarhanuddin <kazifarhan199@gmail.com>
2017-11-15 21:14:54 +0530

    updated

--- Cx_freeze/Cx_freeze(setup file).py
+++ Cx_freeze/Cx_freeze(setup file).py
@@ -1,5 +1,6 @@
 #you need to put it in same dir or use the path
 from cx_Freeze import setup, Executable
 
-setup(name='my exe',version='0.1',description='stuff',executables=[Executable('1ex.py')])         #could use path in Executable('path')
+setup(name='myexe',version='0.1',description='stuff',executables=[Executable('studier_no_gui.py')])         #could use path in Executable('path')
+
 # should ron it with command "python setup.py(is thats the name of this setup) buil"
