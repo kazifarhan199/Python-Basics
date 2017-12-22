6b5a499ac03a693ed39a9cb8401255882d016488
KaziFarhanuddin <kazifarhan199@gmail.com>
2017-12-23 01:00:22 +0530

    update

--- Kivy_basics/basic_kivy.py
+++ Kivy_basics/basic_kivy.py
@@ -9,7 +9,7 @@ class login_screen(GridLayout):
 	def __init__(self,**kwargs):
 		super(login_screen,self).__init__(**kwargs)
 		self.cols=2
-		
+
 		self.add_widget(Label(text="Name: "))
 		self.username=TextInput(multiline=False)
 		self.add_widget(self.username)
@@ -17,8 +17,8 @@ class login_screen(GridLayout):
 		self.add_widget(Label(text="Passward: "))
 		self.password=TextInput(multiline=False,password=True)
 		self.add_widget(self.password)
-		
-		
+
+
 class kivy_app(App):
 	def build(self):
 		return login_screen()
--- flask_basics/Flask_basics_1.py
+++ flask_basics/Flask_basics_1.py
@@ -1,10 +1,13 @@
 from flask import Flask
 
 app = Flask(__name__)
+@app.route('/upload/<filename>')
+def send_image(filenames):
+	return send_from_directory('images',filename)
 
 @app.route('/')
 def homepage():
-	return "<h1> What is your naem ?<h1><h2> Name of you'r school/coledge <h2>"	
+	return "<h1> What is your naem ?<h1><h2> Name of you'r school/coledge <h2>"
 
 if __name__=='__main__':
 	app.run(debug=True)
